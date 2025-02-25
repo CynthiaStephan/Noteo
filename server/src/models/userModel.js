08eb7cb0f2fac54129ddad4ac961ff5beee53f98
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2025-02-25 15:11:41 +0100

    add user model

---
+++ server/src/models/userModel.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const User = sequelize.define('user', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        isEmail: true,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM(),
+        values: ['intern', 'trainer', 'admin'],
+        defaultValue: 'intern',
+        allowNull: false,
+    },
+
+},{
+    tableName: 'user',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: false
+});
+
+module.exports = User;
