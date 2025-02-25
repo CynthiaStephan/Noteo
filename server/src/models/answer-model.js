4db69350704017288fe229994da7967b73e3e4f5
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2025-02-25 15:48:20 +0100

    answer model

---
+++ server/src/models/answer-model.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Answer = sequelize.define('answer', {
+    answer_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    answer: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    }
+},{
+    tableName: 'answer',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: false
+});
+
+module.exports = Answer;
