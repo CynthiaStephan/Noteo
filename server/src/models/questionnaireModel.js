e4dded004cefddeb4423758355747d9366a6dc2e
Dovaaa <pierremarielebas1@gmail.com>
2025-02-25 16:11:14 +0100

    Modification questionnaireModel.js

--- server/src/models/questionnaireModel.js
+++ server/src/models/questionnaireModel.js
@@ -14,10 +14,6 @@ const Questionnaire = sequelize.define('questionnaire', {
     description: {
         type: DataTypes.STRING(500),
     },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    }
 },{
     tableName: 'questionnaire',
     timestamps: true,
