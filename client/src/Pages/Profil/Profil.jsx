64727d94b8b2e7fee2400174c596192bfa5583b8
jordan bayer <jordan.bayer@institutsolacroup.com>
2025-02-25 15:48:04 +0100

    ajout profil

---
+++ client/src/Pages/Profil/Profil.jsx
@@ -0,0 +1,11 @@
+import { NavProfil } from '../../Nav/NavProfil'
+import './Profil.css'
+
+export const Profil = () => {
+    return (
+        <>
+            <NavProfil />
+            <h2>Profil</h2>
+        </>
+    )
+}
