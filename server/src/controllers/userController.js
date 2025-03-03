7afa0263bbe6c08a02137282566d772a72342881
Dovaaa <pierremarielebas1@gmail.com>
2025-03-03 11:24:23 +0100

    Ajout fonction createUser dans le userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,7 @@
 const UserModel = require('../models/userModel');
 
+const bcrypt = require('bcrypt');
+
 class UserController {
     async getUserById(req, res) {
         res.json({message: "GetUserById test reussi"});
@@ -8,16 +10,33 @@ class UserController {
         res.json({message : "GetUsers test reussi"});
     };
     async createUser(req, res) {
-        const user = await UserModel.create({
-            first_name: "test",
-            last_name: "test",
-            email: "teihihihst@gmail.com",
-            password: "test"
-        });
-        res.json({
-            message: "createUser test reussi",
-            user: user
-        });
+        const { first_name, last_name, email, password, role} = req.body;
+        try {
+            const saltRounds = 10;
+
+            let hashedPassword = "";
+            if (password) {
+                hashedPassword = await bcrypt.hash(password, saltRounds);
+            };
+
+            const newUser = await UserModel.create({
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                password: hashedPassword,
+                role: role
+            })
+            let newuserData = {
+                user_id: newUser.dataValues.user_id,
+                first_name: newUser.dataValues.first_name,
+                last_name: newUser.dataValues.last_name,
+                email: newUser.dataValues.email,
+                role: newUser.dataValues.role
+            }
+            res.status(201).json(newuserData)
+        } catch (error) {
+            res.status(500).json({ error : error.message});
+        }
     };
     async updateUser(req, res) {
         res.json({message: "updateUser test reussi"});
