6e4caffe5a622db22e8625b0292e34bd40e9be5e
Dovaaa <pierremarielebas1@gmail.com>
2025-03-04 13:49:54 +0100

    Ajout getUserById dans le userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,10 +4,34 @@ const bcrypt = require('bcrypt');
 
 class UserController {
     async getUserById(req, res) {
-        res.json({message: "GetUserById test reussi"});
+        const { user_id } = req.params;
+
+        try {
+            const user = await UserModel.findByPk(user_id, {
+                attributes: { exclude: "password"},
+            })
+            if (!user || user.length === 0) {
+                return res.status(404).json({error: "Pas de user trouvé"});
+            }
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
     };
+
     async getUsers(req, res) {
-        res.json({message : "GetUsers test reussi"});
+        try {
+            const users = await UserModel.findAll({
+                attributes: { exclude: "password"}
+            });
+            console.log(users);
+            if(!users || users.length === 0){
+                return res.json(message = "Pas de users trouvé");
+            }
+            res.status(200).json(users);
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
     };
     async createUser(req, res) {
         const { first_name, last_name, email, password, role} = req.body;
