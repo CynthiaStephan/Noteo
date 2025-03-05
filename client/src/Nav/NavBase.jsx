34ea2948e4c1bbc535f066b17c76ac42c3283fe7
jordan bayer <jordan.bayer@institutsolacroup.com>
2025-03-05 11:33:12 +0100

    creation de question + logout + alert de succes/erreur

--- client/src/Nav/NavBase.jsx
+++ client/src/Nav/NavBase.jsx
@@ -14,20 +14,33 @@ export const NavBase = () => {
             menuBurger.style.display = 'flex'
         }
     }
-    addEventListener('resize', () =>{
+    addEventListener('resize', () => {
         const menuBurger = document.querySelector('.displayMobile')
-        if (window.screen.width >= 670){
+        if (window.screen.width >= 670) {
             menuBurger.style.display = 'flex';
         }
-        else{
+        else {
             menuBurger.style.display = 'none';
         }
     })
 
     const navigate = useNavigate()
-    
 
-    const btnDeconnexion = () => { navigate('/') }
+
+    const btnDeconnexion = () => {
+        fetch(`http://localhost:5000/auth/logout`, {
+            method: "POST",
+            credentials: 'include',
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data.message === "Successfully logged out" || data.message === "You're already logout") {
+                    navigate('/')
+                }
+            })
+            .catch(error => console.error(error))
+    }
     const btnProfil = () => { navigate('/profil') }
 
     return (
