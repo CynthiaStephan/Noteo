61404476b06661f1ade09d663e0f4af1a5000526
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2025-03-07 14:55:52 +0100

    add data

--- data/date.sql
+++ data/date.sql
@@ -3,3 +3,9 @@ INSERT INTO `user` VALUES (1,'Jean','Dupont','jean@mail.com','$2b$10$7cIPSYYlzHL
 
 -- Insert questions into DB
 INSERT INTO `question` VALUES (1,'Êtes-vous ponctuel en cours ?','2025-03-07 10:14:46','2025-03-07 10:14:46'),(2,'Maîtrisez-vous les bases de JavaScript ?','2025-03-07 10:14:56','2025-03-07 10:14:56'),(3,'Savez-vous résoudre un exercice seul ?','2025-03-07 10:15:02','2025-03-07 10:15:02'),(4,'Respectez-vous les délais ?','2025-03-07 10:15:08','2025-03-07 10:15:08'),(5,'Savez-vous utiliser Git ?','2025-03-07 10:15:18','2025-03-07 10:15:18'),(6,'Comprenez-vous `let`, `const` et `var` ?','2025-03-07 10:15:24','2025-03-07 10:15:24'),(7,'Travaillez-vous bien en équipe ?','2025-03-07 10:15:40','2025-03-07 10:15:40'),(8,'Savez-vous manipuler le DOM ?','2025-03-07 10:15:52','2025-03-07 10:15:52'),(9,'Pouvez-vous écrire une fonction simple ?','2025-03-07 10:15:58','2025-03-07 10:15:58'),(10,'Savez-vous corriger vos erreurs ?','2025-03-07 10:16:06','2025-03-07 10:16:06');
+
+INSERT INTO `questionnaire` VALUES (1,'Super questionnaire',NULL,'2025-03-07 13:53:21',2),(2,'Questionnaire',NULL,'2025-03-07 13:54:09',4);
+
+INSERT INTO `questionnaire_question` VALUES ('2025-03-07 13:53:21',1,2),('2025-03-07 13:53:21',1,4),('2025-03-07 13:53:21',1,7),('2025-03-07 13:54:09',2,1),('2025-03-07 13:54:09',2,3),('2025-03-07 13:54:09',2,8),('2025-03-07 13:54:09',2,9);
+
+INSERT INTO `questionnaire_user` VALUES ('2025-03-07 13:53:21',1,1),('2025-03-07 13:54:09',2,2),('2025-03-07 13:53:21',3,1),('2025-03-07 13:54:09',4,2),('2025-03-07 13:53:21',5,1),('2025-03-07 13:54:09',6,2);
