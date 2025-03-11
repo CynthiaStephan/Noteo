b58d477fcc35bcfd5edd75cecfbdfa851537b4ec
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2025-03-11 11:54:28 +0100

    récupérer les questionnaires d'un utilisateur

--- server/src/routes/questionnaireRoutes.js
+++ server/src/routes/questionnaireRoutes.js
@@ -4,6 +4,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', QuestionnaireController.getAllQuestionnaires);
+router.get('/user/:user_id', QuestionnaireController.getQuestionnaireByUserId);
 router.get('/:questionnaire_id', QuestionnaireController.getQuestionnaireById);
 router.post('/new/:trainer_id', QuestionnaireController.createQuestionnaire);
 router.delete('/:questionnaire_id', QuestionnaireController.deleteQuestionnaire);
